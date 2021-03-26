5fd4b7124384cac61738213297185250bed82a42
03williammoses <03williammoses@gmail.com>
2021-03-26 18:31:55 +0530

    Neutral country printing order fix

--- src/main/java/aatral/warzone/statePattern/GamePlay.java
+++ src/main/java/aatral/warzone/statePattern/GamePlay.java
@@ -2,6 +2,7 @@ package aatral.warzone.statePattern;
 
 import java.util.Map;
 
+import aatral.warzone.gameplay.GameEngine;
 import aatral.warzone.gameplay.GamePlayer;
 import aatral.warzone.model.Countries;
 
@@ -30,6 +31,19 @@ public abstract class GamePlay extends Phase {
 				for (Countries l_co : ((GamePlayer) l_gamePlayer.getValue()).getListOfCountries()) {
 					System.out.printf("%-14s%-40s%-12s%-32s\n", "\n" + l_co.getCountryId(), l_co.getCountryName(),
 							l_co.getArmies(), l_gamePlayer.getKey());
+				}
+				System.out.println();
+			}
+			System.out.println(
+					"---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			System.out.printf("%-14s%-40s%-12s%-20s%-100s\n", "Country ID", "Country Name", "Armies", "Owner",
+					"Bordering Countries");
+			System.out.println(
+					"---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			if(GameEngine.l_neutralCountries.size()!=0) {
+				for (Countries l_co : GameEngine.l_neutralCountries) {
+					System.out.printf("%-14s%-40s%-12s%-20s%-100s\n", "\n" + l_co.getCountryId(), l_co.getCountryName(),
+							l_co.getArmies(), "Neutral", gameEngine.countriesUnderPlayerAsString(l_co));
 				}
 				System.out.println();
 			}
